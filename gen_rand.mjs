2822e8b7e1224e096ce6b5ccf45cc67655b76293
Spencer Colton <me@scolton.tech>
2021-09-24 10:29:42 -0500

    Working on JS

--- gen_rand.mjs
+++ gen_rand.mjs
@@ -1,7 +1,7 @@
 import fs   from 'fs/promises';
 
-const ARRAY_SIZE    = 106_490;
-const ELEMENT_SIZE  = 'byte';
+const DEFAULT_ARRAY_SIZE   = 106_490;
+const DEFAULT_ELEMENT_SIZE = 'byte';
 
 const SIZE_MAP = {
   byte: 8,
@@ -9,10 +9,10 @@ const SIZE_MAP = {
   long: 32
 };
 
-const element_bits  = () => SIZE_MAP[ELEMENT_SIZE];
+const element_bits  = sz => SIZE_MAP[sz];
 const bit_to_byte   = byte => byte >> 3;
-const element_bytes = () => bit_to_byte(element_bits());
-const array_bytes   = () => ARRAY_SIZE * element_bytes();
+const element_bytes = sz => bit_to_byte(element_bits(sz));
+const array_bytes   = (asz, esz) => asz * element_bytes(esz);
 
 const HEADER =
 `\t\t\t.cdecls C,LIST,"msp430.h"
@@ -23,6 +23,19 @@ const HEADER =
 const display = num => ("   " + num).slice(-4);
 
 const main = async () => {
+  let ARRAY_SIZE   = parseInt(process.argv[2]);
+  let ELEMENT_SIZE = process.argv[3];
+
+  if (isNaN(ARRAY_SIZE)) {
+    console.warn(`Specified array size ${process.argv[2]} could not be parsed; using default of ${DEFAULT_ARRAY_SIZE} elements`);
+    ARRAY_SIZE = DEFAULT_ARRAY_SIZE;
+  }
+
+  if (!(ELEMENT_SIZE in SIZE_MAP)) {
+    console.warn(`Specified element size ${ELEMENT_SIZE} invalid, using default of ${DEFAULT_ELEMENT_SIZE}.`);
+    ELEMENT_SIZE = DEFAULT_ELEMENT_SIZE;
+  }
+
   const numbers = [];
 
   for (let i = 0; i < ARRAY_SIZE; ++i)
@@ -40,10 +53,10 @@ const main = async () => {
   const data_output = sections.join(`\n\t\t\t.${ELEMENT_SIZE}`);
 
   const arr_size     = `ARR_SIZE\t.field ${ARRAY_SIZE}, 20`;
-  const el_size      = `EL_SIZE\t\t.byte ${element_bits()}\t\t;; bits`;
+  const el_size      = `EL_SIZE\t\t.byte ${element_bits(ELEMENT_SIZE)}\t\t;; bits`;
 
   const input_array  = `INPUT_ARR\t.${ELEMENT_SIZE}${data_output}`;
-  const output_array = `OUTPUT_ARR\t.space ${array_bytes()}`;
+  const output_array = `OUTPUT_ARR\t.space ${array_bytes(ARRAY_SIZE, ELEMENT_SIZE)}`;
 
   const output = `${HEADER}\n${arr_size}\n${el_size}\n\n${input_array}\n\n${output_array}`;
   
